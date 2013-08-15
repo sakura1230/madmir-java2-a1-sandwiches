2531fc72ca8710b339f8cbfb8220720cf85210ff
clydewbrown <clydewbrown@gmail.com>
2013-08-15 07:55:24 -0400

    Source files for Java 2, Assignment #1

---
+++ src/Sandwich.java
@@ -0,0 +1,16 @@
+
+public class Sandwich {
+
+	/* 
+	 * This is the Sandwich class.
+	 * This is an object class.
+	 * There will be no main() method here
+	 * Instead, you will declare variables to create
+	 * the object's "fields", and then add methods 
+	 * to retrieve or change the value of each field.
+	 * 
+	 */
+
+
+
+}
---
+++ src/TestSandwich.java
@@ -0,0 +1,21 @@
+
+public class TestSandwich {
+
+	public static void main(String[] args) {
+		/*
+		 * This class is a program class.
+		 * It WILL include a main() method.
+		 * This program should create two instances of
+		 * Sandwich, and it should demonstrate the
+		 * use of all of the methods encapsulated
+		 * in the Sandwich class.
+		 * 
+		 * Finish off this program by displaying the
+		 * values of each of the fields for each 
+		 * Sandwich instance.
+		 * 
+		 */
+
+	}
+
+}
