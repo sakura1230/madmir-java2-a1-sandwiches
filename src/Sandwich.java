7e2afb81f1675d48f33fea98380a35efdc42ecef
sakura1230 <cherrysavon@hotmail.com>
2013-08-15 15:09:47 -0400

    This is assignment 1!

--- src/Sandwich.java
+++ src/Sandwich.java
@@ -1,16 +1,29 @@
-
 public class Sandwich {
-
-	/* 
-	 * This is the Sandwich class.
-	 * This is an object class.
-	 * There will be no main() method here
-	 * Instead, you will declare variables to create
-	 * the object's "fields", and then add methods 
-	 * to retrieve or change the value of each field.
-	 * 
-	 */
-
-
-
+	
+	static String mainIngred;
+	static String breadType;
+	static double price;
+	
+	public String getMainIngred() {
+		return mainIngred;
+	}
+	
+	public String getBreadType() {
+		return breadType;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setMainIngred(String newMainIngred) {
+		mainIngred = newMainIngred;
+	}
+	
+	public void setBreadType(String newBreadType) {
+		breadType = newBreadType;
+	}
+	
+	public void setPrice(double newPrice) {
+		price = newPrice;
+	}
 }
--- src/TestSandwich.java
+++ src/TestSandwich.java
@@ -1,21 +1,46 @@
+import javax.swing.JOptionPane;
+
 
 public class TestSandwich {
 
 	public static void main(String[] args) {
-		/*
-		 * This class is a program class.
-		 * It WILL include a main() method.
-		 * This program should create two instances of
-		 * Sandwich, and it should demonstrate the
-		 * use of all of the methods encapsulated
-		 * in the Sandwich class.
-		 * 
-		 * Finish off this program by displaying the
-		 * values of each of the fields for each 
-		 * Sandwich instance.
-		 * 
-		 */
-
+		
+		//Instantiating two Sandwich objects
+		Sandwich mySandwich = new Sandwich();
+		Sandwich yourSandwich = new Sandwich();
+		String mainIngred, breadType;
+		double price;		
+		
+		//Setting the values of fields in Sandwich
+		mainIngred = JOptionPane.showInputDialog("What do you want for the main ingredient?");
+		mySandwich.setMainIngred(mainIngred);
+		
+		breadType = JOptionPane.showInputDialog("What type of bread do you want?");
+		mySandwich.setBreadType(breadType);
+		
+		price = Double.parseDouble(JOptionPane.showInputDialog("What is the price of the sandwich?"));
+		mySandwich.setPrice(price);
+		
+		//Output the details of Sandwich
+		JOptionPane.showMessageDialog(null, "You are getting a "
+				+ mySandwich.getMainIngred()
+				+ " sandwich on " + mySandwich.getBreadType() + " at " + mySandwich.getPrice());
+		
+		//Setting the values of fields in Sandwich
+		mainIngred = JOptionPane.showInputDialog("What do you want for the main ingredient?");
+		yourSandwich.setMainIngred(mainIngred);
+		
+		breadType = JOptionPane.showInputDialog("What type of bread do you want?");
+		yourSandwich.setBreadType(breadType);
+		
+		price = Double.parseDouble(JOptionPane.showInputDialog("What is the price of the sandwich?"));
+		yourSandwich.setPrice(price);
+		
+		//Output the details of Sandwich
+		JOptionPane.showMessageDialog(null, "You are getting a "
+				+ yourSandwich.getMainIngred()
+				+ " sandwich on " + yourSandwich.getBreadType() + " at " + yourSandwich.getPrice());
+		
 	}
 
 }
